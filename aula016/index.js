efb71e8da33db65b5c45798c85d2e5ccd811f11c
GuiM <Guilherme.Marrafon1@gmail.com>
2025-03-11 19:32:33 -0300

    Create index.js

---
+++ aula016/index.js
@@ -0,0 +1,39 @@
+//  array         0       1       2          3       4
+const alunos = ['Luis','Maria','Jonas'] // Luiza, Eduardo
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+//alunos.push('luiza')
+//alunos.push('Eduardo')
+
+//console.log(alunos.slice(0,-2))
+
+
+
+//delete alunos[1] // deleta o elemento da posição 1, que é 'Maria'
+//console.log(alunos[1]) // undefined, pois o elemento foi deletado
+
+//const removido = alunos.shift()
+//console.log(removido)
+//console.log(alunos)
+
+//const removido = alunos.shift() //retira o primeiro elemento da array
+//const removido = alunos.pop() // retira o último elemento da array
+
+//alunos.unshift('Luiza') // adiciona no início da lista
+//alunos.unshift('Fabio') // adiciona no início da lista
+
+//alunos.push('Luiza') // adiciona um elemento no final do array
+//alunos.push('Fabio') // adiciona um elemento no final do array
+
+//alunos[alunos.length] = 'Luiza' //adiciona no final do array
+//alunos[alunos.length] = 'Fabio'
+//alunos[alunos.length] = 'Luana' 
+
+//alunos[0] = 'Eduardo' //altera o valor da posição 0
+//alunos[3] = 'Luiza'  // adcionando
+
+//console.log(alunos)
+//console.log(alunos[0])
+//console.log(alunos[2])
