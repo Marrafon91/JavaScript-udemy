3a1fa7cfe3db9c8e2d4b72eb8dcc7861f8411db0
GuiM <Guilherme.Marrafon1@gmail.com>
2025-03-11 15:50:19 -0300

    Create index.js

---
+++ aula015/index.js
@@ -0,0 +1,9 @@
+console.log(!!(100 / 0))
+//let num2 = Math.floor(num1) // aredonda para baixo
+//let num2 = Math.ceil(num1) // aredonda para cima
+//let num2 = Math.round(num1) // aredonda para o número mais próximo
+
+//console.log(Math.max(1,2,3,4,5,6,7,8,9,-10,1500)) // maior valor
+//console.log(Math.min(1,2,3,4,5,6,7,8,9,-10,1500))// menor valor
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5) // gera um número aleatório entre 5 e 10
+
