c297a95b3801fd37692b80676264428a6c927d02
GuiM <Guilherme.Marrafon1@gmail.com>
2025-03-12 19:58:03 -0300

    Create index.js

---
+++ aula017/index.js
@@ -0,0 +1,15 @@
+//function soma(x = 1, y = 1) {
+//    const resultado = x + y
+//    return resultado
+//}
+
+//const resultado = soma()
+//console.log(resultado)
+
+const raiz = (n) => {
+   return n ** 0.5
+}
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(49))
