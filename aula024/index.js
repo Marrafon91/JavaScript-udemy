e22491edb69873315689be14a8b1c844cebcd5be
GuiM <Guilherme.Marrafon1@gmail.com>
2025-03-15 15:43:44 -0300

    Summary

---
+++ aula024/index.js
@@ -0,0 +1,12 @@
+const hora = 10
+
+ // if pode ser usdo sozinho
+if (hora >= 0 && hora <=  11) {
+    console.log('Bom dia!')
+} else if ( hora >= 12 && hora <= 17) {
+    console.log ('Boa tarde!')
+} else if (hora >= 18 && hora <= 23) {
+    console.log ('Boa noite!')
+} else {
+    console.log ('Esta Hora nao existe!')
+}
