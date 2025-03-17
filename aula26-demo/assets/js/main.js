059e86b1904dae0a6c5b56a82c81f2fd711aee06
GuiM <Guilherme.Marrafon1@gmail.com>
2025-03-17 12:10:44 -0300

    Summary

---
+++ aula26-demo/assets/js/main.js
@@ -0,0 +1,44 @@
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", function (e) { 
+    e.preventDefault() 
+    const inputPeso = e.target.querySelector('#peso')
+    const inputAltura = e.target.querySelector('#altura')
+    
+    const peso = Number(inputPeso.value)
+    const altura = Number(inputAltura.value)
+
+    if(!peso) {
+        setResultado('Peso inválido', false)
+        return
+    }
+
+    if(!altura) {
+        setResultado('Altura inválido', false)
+        return
+    }
+    
+    const imc = getImc(peso, altura)
+
+    console.log(imc)
+});
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+
+}
+
+function criaP() {
+  const p = document.createElement('p')
+  return p
+}
+
+function setResultado (msg, isValid) {
+  const resultado = document.querySelector('#resultado')
+  resultado.innerHTML = ''
+
+  const p = criaP()
+  p.innerHTML = msg
+  resultado.appendChild(p)
+}
