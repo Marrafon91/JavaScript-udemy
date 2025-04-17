ee401fd259dca4c59931bf26ccf4272b055988ec
GuiM <Guilherme.Marrafon1@gmail.com>
2025-04-16 21:44:40 -0300

    Summary

---
+++ aula000Praticas/Ex-array/06_Array_Elementos.js
@@ -0,0 +1,22 @@
+// Crie dois arrays, um com mais de 5 elementos e outro com menos;
+// Faca uma funcao que verifica o numero de elementos;
+// Se possuir menos que 5, imprima "Poucos elementos" no console;
+// Se tiver masi, imprima "Muitos elementos"
+
+const carros = ['BMW', 'VW', 'Hyundai','Mercedes','Audi', 'Honda']
+const modelosCarros = ['HB20','Audi Q3', 'Onix']
+
+/* carros.forEach(function(valor, indice) {
+    console.log(valor, indice + 1)
+    }); */
+function verificarCarros(arr) {
+ if(arr.length >= 5){
+    console.log("Muitos elementos")
+ } else {
+    console.log("Poucos elementos")
+ }
+} 
+
+verificarCarros(carros)
+verificarCarros(modelosCarros)
+
---
+++ aula000Praticas/Ex-array/07_Array_Elementos1.js
@@ -0,0 +1,8 @@
+// Crie um array com 5 elementos;
+// faca uma iteracao entre todos eles e imprima no console.log;
+
+const carros = ['BMW', 'VW', 'Hyundai','Mercedes','Audi']
+
+for(let i = 0;i < carros.length; i++) {
+    console.log(carros[i])    
+}
