b9472e3075cae2032f5f647f038f0af7aab467a4
GuiM <Guilherme.Marrafon1@gmail.com>
2025-05-15 14:01:58 -0300

    Summary

--- node/aula014/server.js
+++ node/aula014/server.js
@@ -1,22 +1,37 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
 const app = express();
-const routes = require('./routes');
-const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware')
+const mongoose = require("mongoose");
+
+
+// Corrigido: apenas uma chamada de conexão
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch((e) => {
+    console.error("Erro ao conectar ao MongoDB:", e);
+  });
+
+const routes = require("./routes");
+const path = require("path");
+const { middlewareGlobal } = require("./src/middlewares/middleware");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "public")));
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
 
-app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
-
-// Nossos Proprios Middleware.
+// Nossos próprios middlewares
 app.use(middlewareGlobal);
 app.use(routes);
 
-
-app.listen(3333, () => {
-    console.log('Acesse http://localhost:3333');
-    console.log('Servidor executando na porta 3333');
+// Inicia o servidor quando o app estiver pronto
+app.on("pronto", () => {
+  app.listen(3333, () => {
+    console.log("Acesse http://localhost:3333");
+    console.log("Servidor executando na porta 3333");
+  });
 });
