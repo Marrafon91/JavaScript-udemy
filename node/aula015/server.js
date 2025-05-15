09eb77cd3ee643fdb3c8fe25975130f6d842a290
GuiM <Guilherme.Marrafon1@gmail.com>
2025-05-15 14:57:54 -0300

    Summary

--- node/aula015/server.js
+++ node/aula015/server.js
@@ -4,9 +4,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-
-// Corrigido: apenas uma chamada de conexão
-mongoose.connect(process.env.CONNECTIONSTRING)
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
   .then(() => {
     app.emit("pronto");
   })
@@ -14,12 +13,29 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     console.error("Erro ao conectar ao MongoDB:", e);
   });
 
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+
 const routes = require("./routes");
 const path = require("path");
 const { middlewareGlobal } = require("./src/middlewares/middleware");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret: 'AaAaBbBbCcCcDdDd()',
+  store: MongoStore.create({mongoUrl: process.env.CONNECTIONSTRING}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+});
+app.use(sessionOptions);
+app.use(flash());
 
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
