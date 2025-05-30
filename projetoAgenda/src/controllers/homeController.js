7332db8992bc31ba1e53e09da0a29d728164fe7c
GuiM <Guilherme.Marrafon1@gmail.com>
2025-05-30 15:07:36 -0300

    Summary

--- projetoAgenda/src/controllers/homeController.js
+++ projetoAgenda/src/controllers/homeController.js
@@ -1,12 +1,5 @@
-exports.paginainicial = (req, res) => {
-    res.render('index', {
-        titulo: 'Este será o <span style="color:red;">titulo</span> da página',
-        numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    });
-    return;
+exports.index = (req, res) => {
+    res.render('index');
+   
 };
 
-exports.trataPost = (req, res) => {
-    res.send(req.body);
-    return;
-};
--- projetoAgenda/src/middlewares/middleware.js
+++ projetoAgenda/src/middlewares/middleware.js
@@ -8,10 +8,12 @@ exports.outroMiddleware = (req, res, next) => {
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err && err.code === 'EBADCSRFTOKEN') {
+    if(err) {
         return res.render('404')
     }
 };
+
+// next();
 
 exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
